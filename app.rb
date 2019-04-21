d09ce5ca02dc4db8059181cabc4b561f2918a75a
Konstantin K <konstanin19@mail.ru>
2019-04-21 19:53:24 +0300

    Add new post form

--- app.rb
+++ app.rb
@@ -17,6 +17,28 @@ before do
 end
 
 get '/' do
-	#@results = @db.execute 'select * from Posts order by id desc'
+	#@posts = @db.execute 'select * from Posts order by id desc'
 	erb :index 		
+end
+
+get '/new' do
+  erb :new
+end
+
+post '/new' do
+        content = params[:content]
+        youname = params[:name]
+        if content.length <= 0
+                @error = 'Type post text'
+                return erb :new
+        end
+
+        if youname.length <= 0
+                @error = 'Type you name'
+                return erb :new
+        end
+
+       # @db.execute 'insert into Posts (youname, content, created_date) values (?,?,datetime())',[youname, content]
+        redirect to '/'
+        #erb "You typed: #{youname}, #{content}"
 end
