17de1df5531ac5f0f4a121c2a3eda65181499048
Konstantin K <konstanin19@mail.ru>
2019-04-21 18:26:34 +0300

    Delete migration

--- app.rb
+++ app.rb
@@ -11,6 +11,7 @@ end
 
 class Comment < ActiveRecord::Base
 end
+rend
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
