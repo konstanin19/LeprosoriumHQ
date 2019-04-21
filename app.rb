734b2e8ee1e1426bbfdbd107a243dc8348a53a02
Konstantin K <konstanin19@mail.ru>
2019-04-21 16:52:23 +0300

    initial app.rb and set database

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+require 'IRB'
+
+set :database, "sqlite3:leprosorium.db"
+ 
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
