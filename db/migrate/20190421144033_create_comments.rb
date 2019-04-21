b7d6a118841ee394888d8cc326d4a338dd36e31f
Konstantin K <konstanin19@mail.ru>
2019-04-21 17:55:19 +0300

    Add comments migration

--- db/migrate/20190421144033_create_comments.rb
+++ db/migrate/20190421144033_create_comments.rb
@@ -1,4 +1,11 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
+  	create_table :post do |t|
+	t.text :name
+	t.text :datestamp
+    t.text :text
+    t.text :post_id
+    t.timestamps
+	end
   end
 end
