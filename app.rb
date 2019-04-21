fd5fc8894786b94b147493e10786f95444437205
Konstantin K <konstanin19@mail.ru>
2019-04-21 17:22:01 +0300

    Add post and comment entity

--- app.rb
+++ app.rb
@@ -6,7 +6,11 @@ require 'IRB'
 
 set :database, "sqlite3:leprosorium.db"
  
+class Post <ActiveRecord::Base
+end
 
+class Comment < ActiveRecord::Base
+end
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
